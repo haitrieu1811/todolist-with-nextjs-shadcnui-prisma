54891eab0f266bd287f5c322cde01874dc6b4522
Tran Hai Trieu <haitrieu2524@gmail.com>
2023-12-02 22:59:29 +0700

    Tạo giao diện form thêm todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,57 @@
+import TodoItem from "@/components/todo-item";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
-      <Button>CLick me</Button>
+    <main className="h-screen w-1/3 mx-auto py-10">
+      <h1 className="font-bold text-3xl text-center">Todo list</h1>
+      <Separator className="my-6" />
+      <div className="space-y-4">
+        <Input type="text" placeholder="Thêm việc cần làm" className="mt-10" />
+        <Select>
+          <SelectTrigger>
+            <SelectValue placeholder="Chọn mức độ công việc" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">
+              <span className="text-blue-500 font-medium">Thong thả</span>
+            </SelectItem>
+            <SelectItem value="medium">
+              <span className="text-yellow-500 font-medium">Bình thường</span>
+            </SelectItem>
+            <SelectItem value="heavy">
+              <span className="text-red-500 font-medium">Deadline</span>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+        <Button className="bg-blue-600">Thêm</Button>
+      </div>
+      <Tabs defaultValue="all" className="mt-6">
+        <TabsList>
+          <TabsTrigger value="all">Tất cả</TabsTrigger>
+          <TabsTrigger value="unfinished">Chưa thực hiện</TabsTrigger>
+          <TabsTrigger value="finished">Đã thực hiện</TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          {Array(20)
+            .fill(0)
+            .map((_, index) => (
+              <TodoItem key={index} />
+            ))}
+        </TabsContent>
+        <TabsContent value="unfinished">Chưa thực hiện.</TabsContent>
+        <TabsContent value="finished">Đã thực hiện.</TabsContent>
+      </Tabs>
     </main>
   );
 }
